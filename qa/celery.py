47f91735eb0f4eda28db27cbe278f31e5626d3ca
alamin <malaminmiu@gmail.com>
2026-01-20 15:06:46 +0600

    up

--- qa/celery.py
+++ qa/celery.py
@@ -4,7 +4,7 @@ from celery import Celery
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qa.settings')
 
-app = Celery('titan')
+app = Celery('qa')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
